90a259ec356d2bef0f21ce59031f923345e65f73
MOHAMMAD AFZAL KARIM <77576898+mak9093@users.noreply.github.com>
2022-06-05 20:55:49 +0530

    Add files via upload

---
+++ style-switcher.js
@@ -0,0 +1,5 @@
+/*toggle style behaviour*/
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
