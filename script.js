0ee0570e3bdb979c8a3408330a8b58d4ed904364
MOHAMMAD AFZAL KARIM <77576898+mak9093@users.noreply.github.com>
2022-06-06 11:44:39 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+/*typing animation*/
+var typed= new Typed(".typing",{
+    strings:[" ","Programmer"," ","Competitive Coder"," ","web developer"," "],
+    typeSpeed:110,
+    BackSpeed:70,
+    loop:true
+})
